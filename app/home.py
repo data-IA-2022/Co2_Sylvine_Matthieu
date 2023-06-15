23eb5cf480018ef0f9a3f6ffadff01e7bb7bd6cb
Matthieu Ernoux <matt.er41@hotmail.fr>
2023-06-15 14:53:54 +0200

    docker file pls new form

--- app/home.py
+++ app/home.py
@@ -1,9 +1,9 @@
 # Importer les modules Flask
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
+from wtforms import FloatField
 # Créer une instance de l'application Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # Clé secrète pour la protection CSRF
@@ -12,6 +12,8 @@ app.config['SECRET_KEY'] = 'your-secret-key'  # Clé secrète pour la protection
 class MyForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
+    age = SelectField('Age', choices=[('18', '18'), ('19', '19'), ('20', '20')], validators=[DataRequired()])
+    quantity = FloatField('Quantity', validators=[DataRequired(), NumberRange(min=1, max=100)])
     submit = SubmitField('Submit')
 
 # Définir une route pour l'URL racine '/'
@@ -21,11 +23,14 @@ def home():
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
+        age = form.age.data
+        quantity = float(form.quantity.data)  # Convertir la valeur en nombre réel
         # Traitez les données du formulaire comme vous le souhaitez
 
         return "Formulaire soumis avec succès !"
 
     return render_template('index.html', form=form)
+
 
 # Point d'entrée de l'application
 if __name__ == '__main__':
